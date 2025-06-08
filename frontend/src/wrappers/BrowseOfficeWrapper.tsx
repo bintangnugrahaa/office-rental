cb9f3a8ff740c43646de09a1b235e6cb2cdb52f0
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-06-08 13:35:18 +0700

    display office detail data

--- frontend/src/wrappers/BrowseOfficeWrapper.tsx
+++ frontend/src/wrappers/BrowseOfficeWrapper.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import OfficeCard from "../components/OfficeCard";
 import type { Office } from "../types/type";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function BrowseOfficeWrapper() {
   const [offices, setOffices] = useState<Office[]>([]);
@@ -47,7 +48,9 @@ export default function BrowseOfficeWrapper() {
       </h2>
       <div className="grid grid-cols-3 gap-[30px]">
         {offices.map((office) => (
-          <OfficeCard key={office.id} office={office} />
+          <Link key={office.id} to={`/office/${office.slug}`}>
+            <OfficeCard office={office} />
+          </Link>
         ))}
       </div>
     </section>
